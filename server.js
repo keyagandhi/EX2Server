2deae796ec0a912f6e6fdc0d36143dc41807ef9d
Keya <keyagandhi251994@gmail.com>
2019-01-19 12:28:07 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+//http is an object and is rational pattern to create  design server and refer reference to new server object 
+const server = http.createServer(function(req,res){
+     //200 = ALL OK ON SERVER
+
+
+     res.statusCode = 200;
+     res.setHeader('Content-Type', 'text/plain');
+     res.end('Hello World\n');
+});
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+
+  
